9fba9ca2f5b1f914fb38134d355b43c9633b3522
G <shjwin96@gmail.com>
2023-02-15 18:22:24 +0900

    feat: 회원가입시 이메일 및 닉네임 중복확인 버튼UI[ATO-APP-UI 3]

--- flutter/lib/component/account/sign_up/sign_up_email_text_form.dart
+++ flutter/lib/component/account/sign_up/sign_up_email_text_form.dart
@@ -4,7 +4,7 @@ import 'package:ato/utility/validation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SignUpEmailTextForm extends StatefulWidget{
+class SignUpEmailTextForm extends StatefulWidget {
   SignUpEmailTextForm({Key? key, this.controller}) : super(key: key);
 
   final controller;
@@ -14,7 +14,7 @@ class SignUpEmailTextForm extends StatefulWidget{
   State<SignUpEmailTextForm> createState() => _SignUpEmailTextFormState();
 }
 
-class _SignUpEmailTextFormState extends State<SignUpEmailTextForm>{
+class _SignUpEmailTextFormState extends State<SignUpEmailTextForm> {
   String? email;
   bool buttonEnable = false;
 
@@ -33,24 +33,75 @@ class _SignUpEmailTextFormState extends State<SignUpEmailTextForm>{
             SignUpEmailTextForm.buttonStateValue = false;
           }
 
-          if (Validation().validateEmail(email!) != null){
+          if (Validation().validateEmail(email!) != null) {
             buttonEnable = false;
           }
         });
       },
       decoration: InputDecoration(
-        labelText: "이메일",
-        hintText: "이메일을 입력해주세요",
-        enabledBorder:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-        focusedBorder:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-        errorBorder:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-        focusedErrorBorder:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-        prefixIcon: Icon(Icons.account_circle),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
+          labelText: "이메일",
+          hintText: "이메일을 입력해주세요",
+          enabledBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          focusedBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          errorBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          focusedErrorBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          prefixIcon: Icon(Icons.account_circle),
+          suffixIcon: Container(
+            margin: EdgeInsets.only(right: 10),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Color(0XFF000000),
+                  primary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30))),
+              onPressed: buttonEnable
+                  ? () {
+                      var validation = SpringMemberApi()
+                          .emailDuplicateCheck(DuplicateEmailRequest(email!));
+
+                      validation.then((value) {
+                        if (value.success == true) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return CommonAlertDialog(
+                                    title: "✅",
+                                    content: "사용 가능한 이메일입니다.",
+                                    onCustomButtonPressed: () {
+                                      Navigator.pop(context);
+                                    });
+                              });
+                          setState(() {
+                            buttonEnable = false;
+                            SignUpEmailTextForm.buttonStateValue = true;
+                          });
+                        }
+                        if (value.success == false) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return CommonAlertDialog(
+                                    title: "⚠️",
+                                    content: "중복된 이메일입니다.",
+                                    onCustomButtonPressed: () {
+                                      Navigator.pop(context);
+                                    });
+                              });
+                        }
+                      });
+                    }
+                  : null,
+              child: Text("중복 확인", style: TextStyle(color: Color(0xffFAEBD7))),
+            ),
+          ),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
     );
   }
 }
--- flutter/lib/component/account/sign_up/sign_up_nickname_text_form.dart
+++ flutter/lib/component/account/sign_up/sign_up_nickname_text_form.dart
@@ -4,7 +4,7 @@ import 'package:ato/utility/validation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SignUpNicknameTextForm extends StatefulWidget{
+class SignUpNicknameTextForm extends StatefulWidget {
   SignUpNicknameTextForm({Key? key, this.controller}) : super(key: key);
 
   final controller;
@@ -14,7 +14,7 @@ class SignUpNicknameTextForm extends StatefulWidget{
   State<SignUpNicknameTextForm> createState() => _SignUpNicknameTextForm();
 }
 
-class _SignUpNicknameTextForm extends State<SignUpNicknameTextForm>{
+class _SignUpNicknameTextForm extends State<SignUpNicknameTextForm> {
   String? name;
   bool buttonEnable = false;
 
@@ -28,12 +28,12 @@ class _SignUpNicknameTextForm extends State<SignUpNicknameTextForm>{
         setState(() {
           name = text;
 
-          if (Validation().validateNickname(text) == null){
+          if (Validation().validateNickname(text) == null) {
             buttonEnable = true;
             SignUpNicknameTextForm.buttonStateValue = false;
           }
 
-          if (Validation().validateNickname(text) != null){
+          if (Validation().validateNickname(text) != null) {
             buttonEnable = false;
           }
         });
@@ -43,6 +43,56 @@ class _SignUpNicknameTextForm extends State<SignUpNicknameTextForm>{
           labelText: "닉네임",
           hintText: "닉네임을 입력해주세요",
           prefixIcon: Icon(Icons.portrait),
+          suffixIcon: Container(
+            margin: EdgeInsets.only(right: 10),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Color(0XFF000000),
+                  primary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30))),
+              onPressed: buttonEnable
+                  ? () {
+                      var validation = SpringMemberApi().nickNameDuplicateCheck(
+                          DuplicateNicknameRequest(name!));
+
+                      validation.then((value) {
+                        if (value.success == true) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return CommonAlertDialog(
+                                    title: "⚠️",
+                                    content: "사용 가능한 닉네임입니다.",
+                                    onCustomButtonPressed: () {
+                                      Navigator.pop(context);
+                                    });
+                              });
+                          setState(() {
+                            buttonEnable = false;
+                            SignUpNicknameTextForm.buttonStateValue = true;
+                          });
+                        }
+                        if (value.success == false) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return CommonAlertDialog(
+                                    title: "⚠️",
+                                    content: "중복된 닉네임입니다.",
+                                    onCustomButtonPressed: () {
+                                      Navigator.pop(context);
+                                    });
+                              });
+                        }
+                      });
+                    }
+                  : null,
+              child: Text("중복 확인", style: TextStyle(color: Color(0xffFAEBD7))),
+            ),
+          ),
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
     );
   }
